31de318e39cb74d79c05e793e9c1213132e4137c
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-05-23 06:27:45 +0530

    Add files via upload

---
+++ vector/maxmimumsumsubarray (1).cpp
@@ -0,0 +1,47 @@
+//naive
+#include <bits/stdc++.h>
+using namespace std;
+void maxsum(vector<int>&v,int n){
+int res=v[0];
+for(auto it=0;it<n;it++){
+ int curr=0;
+ for(int j=it;j<n;j++){
+ curr=curr+v[it];
+ res=max(res,curr);
+
+ }
+}
+cout<<res;
+}
+int main() {
+int a,b,c,d,e,f,m,n,p;
+cin>>n;//number of elements
+vector<int>v;
+int temp;
+for(int i=0;i<n;i++){
+ cin>>temp;
+ v.push_back(temp);
+}
+maxsum(v,n);
+return 0;
+}
+//pro
+#include <bits/stdc++.h>
+using namespace std;
+void maxsum(vector<int>&v,int n){
+ int res=v[0],maxend=v[0];
+ for(auto it=1;it<n;it++){
+ maxend=max(maxend+v[it],v[it]);
+ res=max(res,maxend);
+ }
+ cout<<res;
+}
+int main() {
+int a,b,c,d,e,f,m,n,p;
+cin>>n;//number of elements
+vector<int>v;
+int temp;
+for(int i=0;i<n;i++){
+ cin>>temp;
+ v.push_back(temp);
+}
