31a3e1ebf566bc39e756cfc83ce2a1c8d84e7656
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-06-06 15:29:37 +0530

    Add files via upload

---
+++ binary-search/threepointers.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+ 
+bool find3Numbers(int A[], int arr_size, int sum) 
+{ 
+	int l, r; 
+
+	sort(A, A + arr_size); 
+
+	for (int i = 0; i < arr_size - 2; i++) { 
+ 
+		l = i + 1; 
+
+		r = arr_size - 1;  
+		// 2 pointers approach after traversing the whole element
+		//just create another function as done in the copy
+		while (l < r) { 
+			if (A[i] + A[l] + A[r] == sum) { 
+				printf("Triplet is %d, %d, %d", A[i], A[l], A[r]); 
+				return true; 
+			} 
+			else if (A[i] + A[l] + A[r] < sum) 
+				l++; 
+			else 
+				r--; 
+		} 
+	} 
+	return false; 
+} 
+
+/* Driver program to test above function */
+int main() 
+{ 
+	int A[] = { 1, 4, 45, 6, 10, 8 }; 
+	int sum = 22; 
+	int arr_size = sizeof(A) / sizeof(A[0]); 
+
+	find3Numbers(A, arr_size, sum); 
+
+	return 0; 
+} 
