6295ce2bd5fa5d332186f05a619b2e3559332f59
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-06-06 09:39:24 +0530

    Add files via upload

---
+++ binary-search/index of last occurrence.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl  "\n"
+using namespace std;
+int solve(vector<ll>&arr,int n,int x){
+    ll low = 0, high = n - 1;
+	while(low <= high){
+		ll mid = (low + high) / 2;
+		if(x > arr[mid]){
+			low = mid + 1;
+		}
+		else if(x < arr[mid]){
+			high = mid - 1;
+		}
+		else{
+			if(mid == n-1 || arr[mid + 1] != arr[mid]){
+				return mid;
+			}
+			else{
+				low=mid+1;
+			}
+		}
+
+	}
+
+	return -1;
+   
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    ll n,m,temp1,temp2,T;
+    vector<ll>v;
+    cin>>m;//number to be searched
+    cin>>n;
+     for(ll i=0;i<n;i++){
+     cin>>temp1;
+     v.push_back(temp1);
+  }
+   cout<<solve(v,n,m);
+   
+}
