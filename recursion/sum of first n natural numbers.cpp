3fbbee19c3b7f62e97ed92e6a31b4aa12ba376f6
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-10 14:11:10 +0530

    Add files via upload

---
+++ recursion/sum of first n natural numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun(int n){
+ if(n==0){
+return 0;
+}
+     return fun(n-1)+n;//adding the previous case
+
+}
+int main(){
+    int x;
+    cin>>x;
+    cout<<fun(x);
+}
